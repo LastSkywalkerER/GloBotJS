c0940e9cc5f7b247b4d1d1050a17ce768b01e80a
LastSkywalkerER <maxdr1998@gmail.com>
2021-02-04 19:59:40 +0300

    main task

--- js/main.js
+++ js/main.js
@@ -8,44 +8,51 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function ifMore (botNumber, userNumber) {
-  if (botNumber > userNumber) return true
-  else return false;
+function checkInput(inputNumber) {
+  if (!isNumber(inputNumber) || inputNumber === '-0' || inputNumber === '+0') {
+    if (inputNumber === null) {
+      alert('Игра окончена');
+      return false;
+    }
+    inputNumber = checkInput(prompt('Введи число!'));
+  }
+  return inputNumber;
 }
 
 function start() {
   let start = prompt('Угадай число от 1 до ' + maxGameValue);
-  
-  while (!isNumber(start)) {
-    if (start === null) {
-      alert('Игра окончена');
-      return false;
-    }
-    start = prompt('Введи число!');
+  let end = checkInput(start);
+  console.log(end);
+  if (end !== false) {
+    return parseInt(end);
   }
-  return parseInt(start);
-}
-
-function isEqual (botNumber, userNumber) {
-  if (botNumber === userNumber) return true;
-  return false;
+  else {
+    return false;
+  }
 }
 
 function GloBotJS(){
-  let botNumber = getRandomInt(maxGameValue + 1);
-  let userNumber = start();
-  while(!isEqual(botNumber, userNumber)) {
-    if (ifMore(botNumber, userNumber)) {
-      alert('Загаданное число больше');
-      userNumber = start();
-    } else if (!ifMore(botNumber, userNumber)) {
-      alert('Загаданное число меньше');
-      userNumber = start();
-    }
+
+  if (botNumber > userNumber) {
+    alert('Загаданное число больше');
+    userNumber = start();
+    if (!userNumber) return false;
+    return GloBotJS();
+  } else if (botNumber < userNumber) {
+    alert('Загаданное число меньше');
+    userNumber = start();
+    if (!userNumber) return false;
+    return GloBotJS();
   }
-  alert('Поздравляю, Вы угадали!!!');
+  
+  return true;
 }
 
 let maxGameValue = 100;
 
-GloBotJS()
+let botNumber = getRandomInt(maxGameValue + 1);
+console.log(botNumber);
+
+let userNumber = start();
+
+if (userNumber !== false && GloBotJS()) alert('Поздравляю, Вы угадали!!!');
